a028df201db9332f49c843ec7fd0812a7beb4d88
Njuguna Philip <philipnjuguna66@gmail.com>
2023-09-07 09:39:51 +0300

    wip: Init commit

--- resources/views/pages/property/single.blade.php
+++ resources/views/pages/property/single.blade.php
@@ -6,85 +6,63 @@
         @meta("title", $page->meta_title)
         @meta("description", $page->meta_description)
     @endpush
-
-
-    <section class="">
+    <div class="bg-white">
         <div class="mx-auto md:w-4/5 lg:max-w-7xl py-8 md:mt-20 md:py-12 ">
             <h1 class="py-4 font-extrabold text-2xl lg:text-4xl text-center uppercase px-8 md:px-0">{{ $page->name }}</h1>
-            <div class="mx-auto lg:max-w-7xl  grid grid-cols-1 md:grid-cols-5 gap-4 md:space-x-2 py-5 ">
-                <div class="col-span-3 px-8 md:px-0">
 
-                    <article class="prose  text-lg  ">
-                        @if(! is_null($page->video_path))
-                            @include('templates.embeded._video_iframe' , [ 'videoUri' =>   $page->video_path, 'autoplay' => true ])
-                        @else
-                            <img class="h-auto w-full max-w-full rounded-lg object-cover object-center"
-                                 src="{{  \Illuminate\Support\Facades\Storage::url($page->featured_image)}}"
-                                 alt="{{ $page->meta_title }}"
-                            >
-                        @endif
-                        <p class="prose  text-lg px-8 md:px-0 ">
-                            {{ str($page->body)->toHtmlString() }}
-                        </p>
+            <div class="col-span-3 px-8 md:px-0">
+
+                <article class="prose  text-lg  ">
+                    @if(! is_null($page->video_path))
+                        @include('templates.embeded._video_iframe' , [ 'videoUri' =>   $page->video_path, 'autoplay' => true ])
+                    @else
+                        <img class="h-auto w-full max-w-full rounded-lg object-cover object-center"
+                             src="{{  \Illuminate\Support\Facades\Storage::url($page->featured_image)}}"
+                             alt="{{ $page->meta_title }}"
+                        >
+                    @endif
 
 
 
 
-                        <div class="mt-2">
-                            @if( ! is_null($page->map)  )
-                                {{  new \Illuminate\Support\HtmlString($page->map) }}
-                            @endif
-                        </div>
-
-                        <div class="mt-2">
-                            @if( ! is_null($page->mutation)  )
-                                <a
-                                    target="_blank"
-                                    class="button text-white px-8 py-1 rounded"
-                                    href="{{ asset(\Illuminate\Support\Facades\Storage::url($page->mutation)) }}">
-
-                                    Click here to view plots on offer
-                                </a>
-
-                            @endif
-                        </div>
 
 
-                    </article>
-                </div>
 
-                <div class="col-span-2 prose  text-lg px-8 md:px-0">
-
-                    <div class="">
-                        <livewire:contact.book-site-visit :page="$page->title"/>
-                    </div>
-
-                    <div class="">
-                        <h3 class="font-semibold text-xl md:text-3xl md:font-extrabold text-center px-2"> Amenities and
-                            Features</h3>
-
-                        @if(is_array($page->amenities))
-                            <ul class="list-decimal mx-4">
-                                @foreach($page->amenities as $amenity => $value)
-
-                                    <li class="px-8 font-semibold">
-                                        {{ str($value)->toHtmlString() }}
-                                    </li>
-
-                                @endforeach
-                            </ul>
-                        @else
-                           <div class="px-8 py-4 prose">
-                               {{ str($page->amenities)->toHtmlString() }}
-                           </div>
-                        @endif
-
-                    </div>
-
-
-                </div>
+                </article>
             </div>
 
+
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+
+                <div>
+                    <p class="prose  text-lg px-8 md:px-0 ">
+                        {{ str($page->body)->toHtmlString() }}
+                    </p>
+
+                    <div class="mt-2">
+                        @if( ! is_null($page->map)  )
+                            {{  new \Illuminate\Support\HtmlString($page->map) }}
+                        @endif
+                    </div>
+
+                    <div class="mt-2">
+                        @if( ! is_null($page->mutation)  )
+                            <a
+                                target="_blank"
+                                class="button text-white px-8 py-1 rounded"
+                                href="{{ asset(\Illuminate\Support\Facades\Storage::url($page->mutation)) }}">
+
+                                Click here to view plots on offer
+                            </a>
+
+                        @endif
+                    </div>
+                </div>
+                <div class="border-b-2 border-primary-500">
+                    <livewire:contact.book-site-visit :page="$page->title"/>
+                </div>
+
+            </div>
 
             <div class=" px-8 md:px-0">
 
@@ -107,11 +85,43 @@
 
                 <h3 class="py-4 mt-3 text-center font-bold text-md md:text-4xl">Similar Projects</h3>
 
-                    <livewire:project.website.similar-project :project="$page"/>
+                <livewire:project.website.similar-project :project="$page"/>
+
+            </div>
+
+
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+
+
+                <div class="border-b-2 border-primary-500">
+                    <livewire:contact.book-site-visit :page="$page->title"/>
+                </div>
+
+                <div class="">
+                    <h3 class="font-semibold text-xl md:text-3xl md:font-extrabold text-center px-2"> Amenities and
+                        Features</h3>
+
+                    @if(is_array($page->amenities))
+                        <ul class="list-decimal mx-4">
+                            @foreach($page->amenities as $amenity => $value)
+
+                                <li class="px-8 font-semibold">
+                                    {{ str($value)->toHtmlString() }}
+                                </li>
+
+                            @endforeach
+                        </ul>
+                    @else
+                        <div class="px-8 py-4 prose">
+                            {{ str($page->amenities)->toHtmlString() }}
+                        </div>
+                    @endif
+
+                </div>
 
             </div>
 
         </div>
-    </section>
+    </div>
 
 </x-guest-layout>
