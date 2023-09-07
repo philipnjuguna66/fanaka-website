3f57d39192df42891d249f1997fa5156528d6c1a
Njuguna Philip <philipnjuguna66@gmail.com>
2023-09-07 09:51:24 +0300

    wip: Init commit

--- resources/views/pages/property/single.blade.php
+++ resources/views/pages/property/single.blade.php
@@ -7,6 +7,8 @@
         @meta("description", $page->meta_description)
     @endpush
     <div class="bg-white">
+
+
         <div class="mx-auto md:w-4/5 lg:max-w-7xl py-8 md:mt-20 md:py-12 ">
             <h1 class="py-4 font-extrabold text-2xl lg:text-4xl text-center uppercase px-8 md:px-0">{{ $page->name }}</h1>
 
@@ -35,8 +37,8 @@
 
             </div>
 
-            <div class="flex justify-center">
-                <p class="prose text-justify px-8 md:px-0 ">
+            <div class="flex">
+                <p class="prose  px-8 md:px-0 ">
                     {{ str($page->body)->toHtmlString() }}
                 </p>
             </div>
@@ -69,72 +71,70 @@
 
             </div>
 
-        </div>
+            <div class=" px-8 md:px-0">
 
-        <div class=" px-8 md:px-0">
+                @if(is_array($page->gallery))
 
-            @if(is_array($page->gallery))
+                    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
 
-                <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+                        @foreach($page->gallery as $gallery)
+                            <div>
 
-                    @foreach($page->gallery as $gallery)
-                        <div>
-
-                            <img class="h-auto w-full max-w-full rounded-lg object-cover object-center"
-                                 src="{{  \Illuminate\Support\Facades\Storage::url($gallery)}}"
-                                 alt="{{ $page->meta_title }}"
-                            >
-                        </div>
-                    @endforeach
-
-                </div>
-            @endif
-
-
-        </div>
-
-
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-
-
-            <div class="border-b-2 border-primary-500">
-                <livewire:contact.book-site-visit :page="$page->title"/>
-            </div>
-
-            <div class="">
-                <h3 class="font-semibold text-xl md:text-3xl md:font-extrabold text-center px-2"> Amenities and
-                    Features</h3>
-
-                @if(is_array($page->amenities))
-                    <ul class="list-decimal mx-4">
-                        @foreach($page->amenities as $amenity => $value)
-
-                            <li class="px-8 font-semibold">
-                                {{ str($value)->toHtmlString() }}
-                            </li>
-
+                                <img class="h-auto w-full max-w-full rounded-lg object-cover object-center"
+                                     src="{{  \Illuminate\Support\Facades\Storage::url($gallery)}}"
+                                     alt="{{ $page->meta_title }}"
+                                >
+                            </div>
                         @endforeach
-                    </ul>
-                @else
-                    <div class="px-8 py-4 prose">
-                        {{ str($page->amenities)->toHtmlString() }}
+
                     </div>
                 @endif
 
+
             </div>
 
+
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+
+
+                <div class="border-b-2 border-primary-500">
+                    <livewire:contact.book-site-visit :page="$page->title"/>
+                </div>
+
+                <div class="">
+                    <h3 class="font-semibold text-xl md:text-3xl md:font-extrabold text-center px-2"> Amenities and
+                        Features</h3>
+
+                    @if(is_array($page->amenities))
+                        <ul class="list-decimal mx-4">
+                            @foreach($page->amenities as $amenity => $value)
+
+                                <li class="px-8 font-semibold">
+                                    {{ str($value)->toHtmlString() }}
+                                </li>
+
+                            @endforeach
+                        </ul>
+                    @else
+                        <div class="px-8 py-4 prose">
+                            {{ str($page->amenities)->toHtmlString() }}
+                        </div>
+                    @endif
+
+                </div>
+
+            </div>
+
+
+            <div class="border-b-2 border-primary-500 shadow-xl bg-white px-8 py-4 rounded-md">
+
+                <h3 class="py-4 mt-3 text-center font-bold text-md md:text-4xl">Similar Projects</h3>
+
+                <livewire:project.website.similar-project :project="$page"/>
+            </div>
+
+
         </div>
-
-
-        <div class="border-b-2 border-primary-500 shadow-xl bg-white px-8 py-4 rounded-md">
-
-            <h3 class="py-4 mt-3 text-center font-bold text-md md:text-4xl">Similar Projects</h3>
-
-            <livewire:project.website.similar-project :project="$page"/>
-        </div>
-
-
-    </div>
     </div>
 
 </x-guest-layout>
