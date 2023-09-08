a24a5f3364a24b45a35a7300dae5325825d4480e
Njuguna Philip <philipnjuguna66@gmail.com>
2023-09-08 11:03:25 +0300

    wip: Init commit

--- resources/views/templates/projects/index.blade.php
+++ resources/views/templates/projects/index.blade.php
@@ -1,4 +1,4 @@
-<div class="  @if($section->extra['bg_white']  ) bg-white @endif py-24 sm:py-32">
+<div class="  @if($section->extra['bg_white']  ) bg-white @endif py-12 sm:py-12">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto max-w-5xl text-center">
             <h1 class="text-2xl  md:text-5xl font-bold tracking-tight text-gray-900 "> {{ str($section->extra['heading'])->toHtmlString() }}</h1>
