f7749a1ef6999108b2b29255f13db0ac2906a844
Njuguna Philip <philipnjuguna66@gmail.com>
2023-09-08 08:16:47 +0300

    wip: Init commit

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -42,7 +42,7 @@
                         <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                             <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
                         </svg>
-                        <a href="https://twitter.com/fanakarealestate" target="_blank">Twitter</a>
+                        <a href="https://twitter.com/@FanakaLtd" target="_blank">Twitter</a>
                     </div>
 
                 </div>
@@ -60,7 +60,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
                         </svg>
-                        <a href=" https://www.youtube.com/@fanakarealestate" target="_blank">Youtube</a>
+                        <a href=" https://www.youtube.com/@FanakaRealEstateLtd" target="_blank">Youtube</a>
                     </div>
 
                 </div>
