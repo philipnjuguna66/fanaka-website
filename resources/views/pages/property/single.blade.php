0518469a373d0fdd5361dfe2acdaa0b6cbe7ca5a
Njuguna Philip <philipnjuguna66@gmail.com>
2023-09-09 12:22:19 +0300

    wip: Init commit

--- resources/views/pages/property/single.blade.php
+++ resources/views/pages/property/single.blade.php
@@ -31,7 +31,7 @@
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4 py-12">
 
               <div>
-                  <p class="prose  px-8 md:px-0 py-2">
+                  <p class="px-8 py-4 prose">
                       {{ str($page->body)->toHtmlString() }}
                   </p>
               </div>
