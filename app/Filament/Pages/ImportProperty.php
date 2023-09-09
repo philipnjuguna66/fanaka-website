61130684d49bdb57245ae2d049bd220d376483ac
Njuguna Philip <philipnjuguna66@gmail.com>
2023-09-09 13:28:27 +0300

    wip: Init commit

--- app/Filament/Pages/ImportProperty.php
+++ app/Filament/Pages/ImportProperty.php
@@ -14,6 +14,8 @@ class ImportProperty extends Page
     protected static string $view = 'filament.pages.import-property';
 
 
+    public $property;
+
 
     protected function getFormSchema(): array
     {
