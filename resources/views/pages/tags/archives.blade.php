8afdc9caee885a1417dc5096e3453a554004c25a
Njuguna Philip <philipnjuguna66@gmail.com>
2023-09-11 16:23:24 +0300

    wip: Init commit

--- resources/views/pages/tags/archives.blade.php
+++ resources/views/pages/tags/archives.blade.php
@@ -7,7 +7,7 @@
                     <h1 class="text-3xl font-bold tracking-tight sm:text-4xl"> {{ $tag->name  }} </h1>
                 </div>
 
-                <livewire:blog.website.tag-posts :tag="$tag"/>
+                <livewire:blog.tag-posts :tag="$tag"/>
 
             </div>
         </div>
