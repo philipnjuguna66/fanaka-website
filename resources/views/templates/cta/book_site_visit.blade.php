276641802a956491160b0108e43cab716de5a39c
Njuguna Philip <philipnjuguna66@gmail.com>
2023-09-13 07:47:22 +0300

    wip: Init commit

--- resources/views/templates/cta/book_site_visit.blade.php
+++ resources/views/templates/cta/book_site_visit.blade.php
@@ -1,7 +1,7 @@
 <section   class=" @if($section->extra['bg_white']  ) bg-white @endif">
         <div class=" mx-auto md:w-4/5 relative isolate  py-12">
             <div  {{ $animationEffect }} class="mx-auto grid max-w-7xl grid-cols-1 md:grid-cols-2 gap-4">
-                <div class="relative px-6 pb-12 pt-12 sm:pt-12 lg:static lg:px-8 lg:py-12 shadow-2xl gap-12  shadow-gray-400  rounded-md ">
+                <div class="relative px-6 pb-12 pt-12 sm:pt-12 lg:static lg:px-8 lg:py-12  ">
 
 
                     <div class="mx-auto max-w-xl lg:mx-0 lg:max-w-lg ">
@@ -26,7 +26,7 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
                                     </svg>
                                 </dt>
-                                <dd><a class="hover:text-gray-900" href="tel:{{ $section->extra['phone_number'] }}">{{ $section->extra['phone_number'] }}</a></dd>
+                                <dd><a class="hover:text-gray-900" href="tel:{{ $section->extra['phone_number'] }}">{!! $section->extra['phone_number'] !!}</a></dd>
                             </div>
                             <div class="flex gap-x-4">
                                 <dt class="flex-none">
@@ -35,12 +35,12 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                                     </svg>
                                 </dt>
-                                <dd><a class="hover:text-gray-900" href="mailto:{{ $section['email'] }}">{{ $section->extra['email'] }}</a></dd>
+                                <dd><a class="hover:text-gray-900" href="mailto:{{ $section['email'] }}">{!! $section->extra['email'] !!}</a></dd>
                             </div>
                         </dl>
                     </div>
                 </div>
-               <div class="relative  px-6 pb-12 pt-12 sm:pt-12 lg:static lg:px-8 lg:py-12 shadow-2xl  shadow-gray-400  rounded-md">
+               <div class="relative  px-6 pb-12 pt-12 sm:pt-12 lg:static lg:px-8 lg:py-12">
                    <livewire:contact.book-site-visit/>
                </div>
             </div>
