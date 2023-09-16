bd6e0cda0e2e19a7a7a0d78cfa02c9f4ff3cfb6b
Njuguna Philip <philipnjuguna66@gmail.com>
2023-09-16 11:51:22 +0300

    wip: Init commit

--- app/Http/Controllers/ShowPageController.php
+++ app/Http/Controllers/ShowPageController.php
@@ -6,6 +6,7 @@ use App\Models\Page;
 use App\Models\Permalink;
 use App\Models\Whatsapp;
 use Appsorigin\Plots\Models\Blog;
+use Appsorigin\Plots\Models\Project;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 
@@ -23,13 +24,19 @@ class ShowPageController extends Controller
         }
 
 
+
         $page = Cache::get($key);
 
-      /*  $whatsApp = Whatsapp::query()
-            ->whereJsonContains('location_tags', $page?->has('branches')?->branches()?->pluck('location_id')->toArray())
-            ->first();
+        if ($page instanceof Project)
+        {
+            $whatsApp = Whatsapp::query()
+      ->whereJsonContains('location_tags', $page?->has('branches')?->branches()?->pluck('location_id')->toArray())
+      ->first();
 
-        Log::info("whas", [$whatsApp, $page?->has('branches')?->branches()?->pluck('location_id')->toArray() ]);*/
+  Log::info("whas", [$whatsApp, $page?->has('branches')?->branches()?->pluck('location_id')->toArray() ]);
+
+        }
+
 
 
 
