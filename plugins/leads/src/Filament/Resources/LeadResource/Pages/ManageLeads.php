765800cea4059aba3744571396e8b0c9558c9ca6
Njuguna Philip <philipnjuguna66@gmail.com>
2023-09-17 11:08:59 +0300

    ->

--- plugins/leads/src/Filament/Resources/LeadResource/Pages/ManageLeads.php
+++ plugins/leads/src/Filament/Resources/LeadResource/Pages/ManageLeads.php
@@ -5,6 +5,7 @@ namespace Appsorigin\Leads\Filament\Resources\LeadResource\Pages;
 use Appsorigin\Leads\Filament\Resources\LeadResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ManageRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ManageLeads extends ManageRecords
 {
@@ -15,5 +16,10 @@ class ManageLeads extends ManageRecords
         return [
            // Actions\CreateAction::make(),
         ];
+    }
+
+    protected function getTableQuery(): Builder
+    {
+        return parent::getTableQuery()->latest();
     }
 }
