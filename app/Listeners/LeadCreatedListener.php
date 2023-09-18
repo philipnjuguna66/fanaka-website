0a269a04da6fc1b82cb2d0ddf00b2ee7596b0ebd
Njuguna Philip <philipnjuguna66@gmail.com>
2023-09-18 08:42:18 +0300

    ->

--- app/Listeners/LeadCreatedListener.php
+++ app/Listeners/LeadCreatedListener.php
@@ -29,6 +29,16 @@ class LeadCreatedListener implements ShouldQueue
     public function handle(LeadCreatedEvent $event)
     {
 
+        $response = Http::post('https://mis.fanaka.co.ke/api/notification', [
+            'tel' => $event->phone,
+            'branch' => $event->branch,
+            'name' => $event->name,
+            'message' => $event->message,
+        ]);
+
+
+
+
 
         (new TelegramBot())->sendMessage($event->message);
 
