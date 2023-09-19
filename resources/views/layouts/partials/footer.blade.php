1c866a080d2b48c00bfc95108c3f5af358904d1d
Njuguna Philip <philipnjuguna66@gmail.com>
2023-09-19 18:09:16 +0300

    ->

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -87,7 +87,7 @@
         <a
             target="_blank"
            href="tel:@yield('whatsApp',  str($whatsApp)->trim()->toString() )."
-            class="open-button button  bg-red-600 mx-auto max-w-4xl fixed block md:hidden left-[calc(42%)] z-50 hover:bg-red-600">
+            class="call-button button  bg-red-600 mx-auto max-w-4xl fixed block md:hidden left-[calc(42%)] z-50 hover:bg-red-600">
 
             Call: @yield('whatsApp',  str($whatsApp)->trim()->toString() )
         </a>
