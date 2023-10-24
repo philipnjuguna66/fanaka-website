beaf7ca8cd99baf28113b7ef950b42dfdb772bf7
Njuguna Philip <philipnjuguna66@gmail.com>
2023-10-24 15:22:31 +0300

    upgrading to v3

--- resources/views/pages/property/single.blade.php
+++ resources/views/pages/property/single.blade.php
@@ -81,7 +81,7 @@
 
                             <div class="md:text-center align-middle">
 
-                                <div class="flex flex-row">
+                                <div class="flex flex-col">
 
                                     <span> Interested in this Project:?</span>
 
