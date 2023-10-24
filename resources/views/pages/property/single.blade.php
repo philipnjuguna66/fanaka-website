88bf178cc19eeb926c5337c913ebfdcc1c5628c1
Njuguna Philip <philipnjuguna66@gmail.com>
2023-10-24 14:18:36 +0300

    upgrading to v3

--- resources/views/pages/property/single.blade.php
+++ resources/views/pages/property/single.blade.php
@@ -13,10 +13,10 @@
         <div class="mx-auto md:w-4/5 max-w-full	 py-12 md:mt-20 md:py-16 px-8">
             <h1 class="py-12 md:py-4 font-extrabold text-2xl lg:text-4xl text-center uppercase px-8 md:px-0">{{ $page->name }}</h1>
 
-            <div class="grid grid-cols-1 md:grid-cols-5 gap-4 py-8 max-w-7xl">
+            <div class="grid grid-cols-1 md:grid-cols-7 gap-4 py-8 max-w-7xl">
 
 
-                <div class="md:col-span-3">
+                <div class="md:col-span-5">
 
                     <article class=" ">
                         @if(! is_null($page->video_path))
