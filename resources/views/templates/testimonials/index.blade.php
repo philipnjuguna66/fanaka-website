524841e74dde952c1ff0dcaaca9487b8b812fa6e
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-16 13:26:50 +0300

    wip

--- resources/views/templates/testimonials/index.blade.php
+++ resources/views/templates/testimonials/index.blade.php
@@ -13,15 +13,19 @@
 
             ?>
             <div class="flex items-center mt-20 py-12  mx-auto max-w-3xl justify-between">
-                <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                </svg>
-                <p class="ms-2 text-sm font-bold dark:text-white">
-                    Google rating score: {{ number_format($place->rating, 1) }}  of 5,based on {{ $place->user_ratings_total }} reviews
 
-                </p>
+                <div>
+                    <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                    </svg>
+                    <p class="ms-2 text-sm font-bold dark:text-white">
+                        Google rating score: {{ number_format($place->rating, 1) }}  of 5,based on {{ $place->user_ratings_total }} reviews
 
-                <a href="https://g.page/r/CaCbaPHmrGpbEB0/review" target="_blank" class="button">Leave Review</a>
+                    </p>
+                </div>
+                <div>
+                    <a href="https://g.page/r/CaCbaPHmrGpbEB0/review" target="_blank" class="button bg-red-600">Leave Review</a>
+                </div>
 
             </div>
 
