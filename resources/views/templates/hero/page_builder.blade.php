c6b616e5d297976f9dafdaf308fcd8b2e008e210
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-19 15:43:31 +0300

    wip

--- resources/views/templates/hero/page_builder.blade.php
+++ resources/views/templates/hero/page_builder.blade.php
@@ -9,7 +9,7 @@
         </div>
 
         <div class="mt-4 py-4">
-            <div class="grid grid-cols-1 md:grid-cols-{{ $section->extra['columns'] }}  gap-x-1 space-y-4">
+            <div class="grid grid-cols-1 md:grid-cols-{{ $section->extra['columns'] }}  gap-x-2 gap-y-2">
                 @foreach($section->extra['columns_sections'] as $index => $columns)
                     <div class="md:text-justify md:max-w-7xl">
                         @foreach($columns as $column)
