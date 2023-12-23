9a09d0c216cb352a426f76d30ae62a10f72c26c1
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-23 18:41:00 +0300

    wip

--- resources/views/templates/slider/index.blade.php
+++ resources/views/templates/slider/index.blade.php
@@ -5,7 +5,7 @@
             @foreach($section->extra['sliders'] as $slider)
                 <a href="{{ url($slider['url'] ?? "#") }}">
 
-                    <div class="hidden duration-700  ease-in-out" data-carousel-item>
+                    <div class=" duration-700  ease-in-out" data-carousel-item>
                         <img  src="{{ \Illuminate\Support\Facades\Storage::url($slider['image']) }}"
                               class="absolute block object-cover bg-center bg-contain bg-no-repeat  w-full object-center -translate-x-1/2 -translate-y-1/2  top-1/2 left-1/2"
                               alt="{{ $page->meta_title }}">
