85af38931754dc13640ed11e6987e15bea80f852
Njuguna Philip <philipnjuguna66@gmail.com>
2024-01-27 19:32:10 +0300

    wip

--- resources/views/pages/locations/archives.blade.php
+++ resources/views/pages/locations/archives.blade.php
@@ -4,7 +4,7 @@
         <div class="mx-auto md:w-4/5">
             <div class="md:mx-auto max-w-7xl">
                 <div class="mx-auto max-w-2xl text-center">
-                    <h1 class="text-2xl font-bold tracking-tight md:text-5xl"> {{ $branch->name  }} Projects for Sale</h1>
+                    <h1 class="text-2xl font-bold tracking-tight md:text-5xl"> {{ $branch->name  }} Plots for Sale With Ready Title Deed </h1>
                 </div>
 
                 <livewire:project.website.location-projects :branch="$branch"/>
