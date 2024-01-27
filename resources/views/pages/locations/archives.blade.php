6e57b3199e3fa04df35c41d7acdd510f4a301061
Njuguna Philip <philipnjuguna66@gmail.com>
2024-01-27 16:52:54 +0300

    wip

--- resources/views/pages/locations/archives.blade.php
+++ resources/views/pages/locations/archives.blade.php
@@ -1,10 +1,10 @@
 <x-guest-layout>
-    @section('title', $branch->name . " projects for Sale")
-    <section class="mt-8  mt-4 py-8     bg-white">
+    @section('title', $branch->name . " Plots for Sale")
+    <section class="py-8 bg-white">
         <div class="mx-auto md:w-4/5">
             <div class="md:mx-auto max-w-7xl">
                 <div class="mx-auto max-w-2xl text-center">
-                    <h1 class="text-3xl font-bold tracking-tight sm:text-4xl"> {{ $branch->name  }} Projects for Sale</h1>
+                    <h1 class="text-2xl font-bold tracking-tight md:text-5xl"> {{ $branch->name  }} Projects for Sale</h1>
                 </div>
 
                 <livewire:project.website.location-projects :branch="$branch"/>
