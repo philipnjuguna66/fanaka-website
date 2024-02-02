17b47a73839c4aa6076bdba50e48aed2d39e4089
Njuguna Philip <philipnjuguna66@gmail.com>
2024-02-02 13:29:15 +0300

    wip: install cache

--- resources/views/templates/embeded/_video_iframe.blade.php
+++ resources/views/templates/embeded/_video_iframe.blade.php
@@ -1,20 +1,29 @@
-
 @php
-    $videoUri = str($videoUri)
-    ->replace("watch?v=", "")
-    ->replace("https://www.youtube.com/", "https://www.youtube.com/embed/")
-    ->replace("https://youtu.be/", "https://www.youtube.com/embed/")
-    ->replace("https://www.youtube.com/embed/embed/", "https://www.youtube.com/embed/")
-    ->replace("https://www.youtube.com/shorts/", "https://www.youtube.com/embed/")
-    ->replace("https://www.youtube.com/embed/shorts/", "https://www.youtube.com/embed/")
-    ->value();
+
+    $styles  = "";
+
+    if (str($videoUri)->contains('shorts'))
+        {
+            $styles = 'width="315" height="560"';
+        }
+
+        $videoUri = str($videoUri)
+        ->replace("watch?v=", "")
+        ->replace("https://www.youtube.com/", "https://www.youtube.com/embed/")
+        ->replace("https://youtu.be/", "https://www.youtube.com/embed/")
+        ->replace("https://www.youtube.com/embed/embed/", "https://www.youtube.com/embed/")
+        ->replace("https://www.youtube.com/shorts/", "https://www.youtube.com/embed/")
+        ->replace("https://www.youtube.com/embed/shorts/", "https://www.youtube.com/embed/")
+        ->value();
 
 @endphp
 
 <iframe
+    {{ $styles }}
     src="{{ $videoUri }}?rel=0&&mute=0&controls=0&autoplay={{ isset($autoplay) ?? 0 }}"
-    class="w-full aspect-video"
+
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+
     allowfullscreen>
 
 </iframe>
