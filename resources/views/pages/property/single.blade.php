b990b143025eea879b225074dc2b8e106553a957
philip Njuguna <philipnjuguna66@gmail.com>
2024-05-17 10:50:17 +0300

    chnage whatsapp number in phone to be randomly selected

--- resources/views/pages/property/single.blade.php
+++ resources/views/pages/property/single.blade.php
@@ -144,11 +144,11 @@
 
                 </div>
 
-                <div class="max-w-7xl">
+                <div class="max-w-7xl ">
 
                     @if(is_array($page->gallery))
 
-                        <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+                        <div class="grid grid-cols-1 md:grid-cols-3 gap-4 hes-gallery">
 
                             @foreach($page->gallery as $gallery)
                                 <div>
