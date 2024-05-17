82d767546ab9e24a11c93d8b5a89530024a00ad3
philip Njuguna <philipnjuguna66@gmail.com>
2024-05-17 10:26:48 +0300

    chnage whatsapp number in phone to be randomly selected

--- app/Http/Livewire/Contact/BookSiteVisit.php
+++ app/Http/Livewire/Contact/BookSiteVisit.php
@@ -46,7 +46,6 @@ class BookSiteVisit extends Component implements HasForms
 
     protected function getFormSchema(): array
     {
-
         return [
             Grid::make(1)
                 ->schema([
