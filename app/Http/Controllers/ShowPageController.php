33c2d3753717945e4b0826780264dc8dc21a4e89
philipnjuguna66 <philipnjuguna66@gmail.com>
2024-05-31 13:31:17 +0300

    whatsapp number

--- app/Http/Controllers/ShowPageController.php
+++ app/Http/Controllers/ShowPageController.php
@@ -50,7 +50,7 @@ class ShowPageController extends Controller
             {
                 $whatsApp = Whatsapp::query()->inRandomOrder()->first();
             }
-            Log::info("Whatsapp", ['Whatsapp' => $whatsApp,'page' => $page]);
+            Log::info("Whatsapp", ['Whatsapp' => $whatsApp->phone_number,'page' => $page]);
 
 
             return view($permalink->type->template(), [
