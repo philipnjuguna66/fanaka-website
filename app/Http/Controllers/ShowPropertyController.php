ba0f8515759d11c86e021d7270899aabcd3361ee
philipnjuguna66 <philipnjuguna66@gmail.com>
2024-05-31 11:49:06 +0300

    whatsapp number

--- app/Http/Controllers/ShowPropertyController.php
+++ app/Http/Controllers/ShowPropertyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Permalink;
 use App\Models\Whatsapp;
+use Appsorigin\Plots\Models\Project;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 
@@ -23,13 +24,33 @@ class ShowPropertyController extends Controller
 
         $views = Cache::get($previewKey, 1);
 
+        $page = Cache::get($previewKey);
 
+        $whatsApp = Whatsapp::query()->inRandomOrder()->first();
+
+        if ($page instanceof Project) {
+
+            $locationIds =  $page->branches()?->pluck('location_id')->toArray();
+
+            $ids = [];
+
+            foreach ($locationIds as $locationId) {
+
+                $ids[] = "$locationId";
+            }
+
+            $whatsApp = Whatsapp::query()
+                ->whereJsonContains('location_tags', $ids)
+                ->first();
+
+        }
 
 
 
         return view('pages.property.single', [
             'page' => $permalink->linkable,
             'views'  => $views,
+            'whatsApp' => $whatsApp
         ]);
     }
 }
