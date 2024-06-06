84ab6c72debdc9df49861d2037bda53ce66defa8
philipnjuguna66 <philipnjuguna66@gmail.com>
2024-06-06 09:04:57 +0300

    featured project section

--- resources/views/templates/projects/index.blade.php
+++ resources/views/templates/projects/index.blade.php
@@ -2,7 +2,7 @@
 <div class="md:mx-auto max-w-7xl px-2 lg:px-8">
         <div class="mx-auto max-w-5xl text-center">
             <h1 class="text-xl  md:text-3xl font-bold tracking-tight"> {{ str($section->extra['heading'])->toHtmlString() }}</h1>
-            <p class="py-3 text-lg leading-8 text-gray-600">{{ str($section->extra['subheading'])->toHtmlString() }}.</p>
+            <p class="py-3 text-lg leading-8 text-gray-600">{{ str($section->extra['subheading'])->toHtmlString() }}</p>
 
         </div>
         <livewire:project.website.list-project :take="$section->extra['count']"/>
