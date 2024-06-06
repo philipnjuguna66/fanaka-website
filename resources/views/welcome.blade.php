fdd6e375ac4695730dc139dc5d6eb6cfd12efb60
philipnjuguna66 <philipnjuguna66@gmail.com>
2024-06-06 10:58:20 +0300

    featured project section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,8 @@
     @endpush
     <div class="mt-0">
         @foreach($page->sections as $section)
-            @php $animationEffect = new \Illuminate\Support\HtmlString('data-aos="fade-right" set="200" data-aos-easing="ease-in-sine" data-aos-duration="600"');
-       if ($loop->even && ($loop->index != 1 )){
+            @php $animationEffect = new \Illuminate\Support\HtmlString('');
+           if ($loop->even && ($loop->iteration != 2  )){
            $animationEffect = new \Illuminate\Support\HtmlString('data-aos="fade-left" set="200" data-aos-easing="ease-in-sine" data-aos-duration="600"'); }
             @endphp
             @include($section->type->sectionPath() ,['section' => $section ,'animationEffect' => $animationEffect])
