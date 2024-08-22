4195f2e0084bfa1e2738d85800c8c48443a04ba7
philipnjuguna66 <philipnjuguna66@gmail.com>
2024-08-22 12:36:55 +0300

    url infull width image

--- resources/views/templates/projects/featured.blade.php
+++ resources/views/templates/projects/featured.blade.php
@@ -2,8 +2,10 @@
 <div class="md:mx-auto max-w-7xl px-2 lg:px-8">
         <div class="mx-auto max-w-5xl text-center">
             <h1 class="text-xl  md:text-3xl font-bold tracking-tight"> {{ str($section->extra['heading'])->toHtmlString() }}</h1>
-            <p class="py-3 text-lg leading-8 text-gray-600">{{ str($section->extra['subheading'])->toHtmlString() }}</p>
 
+        </div>
+        <div class="mx-auto max-w-5xl">
+            <p class="py-3 text-lg leading-8 text-gray-600 sm:text-center">{{ str($section->extra['subheading'])->toHtmlString() }}</p>
         </div>
         <livewire:project.website.featured-project :projectIds="$section->extra['project_ids']"/>
 
