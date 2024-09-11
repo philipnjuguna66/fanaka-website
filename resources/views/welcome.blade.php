1d2780aaf70278d703fd3c7fb02c103c12f59839
philipnjuguna66 <philipnjuguna66@gmail.com>
2024-09-11 09:24:44 +0300

    remove cache

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,14 +9,14 @@
     @endpush
     <div class="mt-0">
 
-        <div class=" ">
-            @foreach($page->sections as $section)
-                @php $animationEffect = new \Illuminate\Support\HtmlString('');
-           if ($loop->even && ($loop->iteration != 2  )){
-               $animationEffect = new \Illuminate\Support\HtmlString('data-aos="fade-left" set="200" data-aos-easing="ease-in-sine" data-aos-duration="600"'); }
-                @endphp
-                @include($section->type->sectionPath() ,['section' => $section ,'animationEffect' => $animationEffect])
-            @endforeach
-        </div>
+        @if(\Illuminate\Support\Facades\Cache::has(\App\Models\Page::CACHE_KEY.".{$page->id}.html"))
+
+            {!! \Illuminate\Support\Facades\Cache::get(\App\Models\Page::CACHE_KEY.".{$page->id}.html") !!}
+        @else
+            @include('layouts.cache.page',['page' => $page])
+
+        @endif
+
+      {{--  --}}
     </div>
 </x-guest-layout>
