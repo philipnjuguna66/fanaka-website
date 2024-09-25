b1f529536a38ed0b57f29dabd471321b2c4afcc3
seph-kihunii <52157289+seph-kihunii@users.noreply.github.com>
2024-09-25 15:27:58 +0300

    feat: added HOD / managers section in our team page.

--- resources/views/templates/teams/management.blade.php
+++ resources/views/templates/teams/management.blade.php
@@ -1,4 +1,4 @@
-<div class="shadow-md rounded-2xl  bg-white mt-6">
+<div class="shadow-md rounded-2xl  bg-primary-500 mt-6">
     <div class="grid grid-cols-1">
         <div class="">
             <img class="rounded-full h-96 w-96 object-cover"
@@ -6,7 +6,7 @@
                  src="{{ \Illuminate\Support\Facades\Storage::url($team->featured_image) }}" alt="{{ $team->name }}">
 
         </div>
-        <div class=" px-4 bg-primary-400">
+        <div class=" px-4 bg-secondary-400">
             <h3 class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-50">
 
                 <a
