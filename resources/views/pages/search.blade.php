e0dcbfc48f3ba316e6cddce64d6ce3e05087dcc7
philipnjuguna66 <philipnjuguna66@gmail.com>
2024-10-07 15:17:20 +0300

    feat: add a search feature

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -7,38 +7,46 @@
         @meta('description', "Affordable Plots for Sale along Thika Road, Kangundo Road, Mombasa Road")
     @endpush
 
+
+
     <section class="py-4">
         <div class="">
             <div class="mx-auto max-w-7xl">
 
-                <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
-                    @foreach($results as $index => $result)
-                        <div class="bg-white rounded-2xl shadow-md">
-                            <a href="{{ $result['url'] }}" target="_blank">
-                                <div>
-                                    <img
-                                        src="{{ $result['featured_image'] }}"
-                                        class="w-full h-[250px] object-cover"
-                                        alt="{{ $result['title'] }}"
-                                    >
-                                    <div class=" px-5 py-2">
-                                        {{ $result['title'] }}
+              @if($results)
+                    <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                        @foreach($results as $index => $result)
+                            <div class="bg-white rounded-2xl shadow-md">
+                                <a href="{{ $result['url'] }}" target="_blank">
+                                    <div>
+                                        <img
+                                            src="{{ $result['featured_image'] }}"
+                                            class="w-full h-[250px] object-cover"
+                                            alt="{{ $result['title'] }}"
+                                        >
+                                        <div class=" px-5 py-2">
+                                            {{ $result['title'] }}
+                                        </div>
                                     </div>
-                                </div>
-                            </a>
+                                </a>
 
-                           <div class="py-4 px-8">
-                               <a
-                                   target="_blank"
-                                   href="{{ $result['url'] }}"
-                                   class="button"
-                               >
-                                   View More Details
-                               </a>
-                           </div>
-                        </div>
-                    @endforeach
-                </div>
+                                <div class="py-4 px-8">
+                                    <a
+                                        target="_blank"
+                                        href="{{ $result['url'] }}"
+                                        class="button"
+                                    >
+                                        View More Details
+                                    </a>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                @else
+                  <div class="h3 mx-auto text-center">
+                      <p>No Result found for {{ old('query') }}</p>
+                  </div>
+              @endif
 
             </div>
         </div>
